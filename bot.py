eef5be6a2f9cdf33f667c19664ee4b8855740011
Joshua360 <joshuaowuor27@gmail.com>
2022-05-26 10:54:55 +0300

    30 minutes sleep time

--- bot.py
+++ bot.py
@@ -24,6 +24,7 @@ for submission in subreddit.hot(limit=2):
             print(comment.body)
             comment.reply("I can vouch for essay-gurus.com, I had a 5pg paper due in 12hrs and they delivered a quality paper.")
             print("\n")
-            # sleep for 1 hour
-            time.sleep(3600)
+            # sleep for 30 minutes
+            time.sleep(1800)
+            # time.sleep(3600)
             print("---------------------------------\n")
